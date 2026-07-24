931ab3a06055676951bd34cb13041efdcf6bdda2
Neemarhh <nibukoye080@gmail.com>
2026-07-24 15:37:18 +0100

    Complete TICKET-01: Calculator

---
+++ BASICS/TICKET-01/code/calculator.js
@@ -0,0 +1,59 @@
+let num1 = 3;
+let num2 = 15;
+
+console.log("Calculator Results (Positive Numbers):");
+// Addition of two positive numbers
+let sum = num1 + num2;
+console.log(`Addition: ${num1} + ${num2} = ${sum}`);
+
+//Subraction of two positive numbers
+let difference = num1 - num2;
+console.log(`Subtraction: ${num1} - ${num2} = ${difference}`);
+
+//Multiplication of two positive numbers
+let product = num1 * num2;
+console.log(`Multiplication: ${num1} * ${num2} = ${product}`);
+
+//Division of two positive numbers
+let quotient = num1 / num2;
+console.log(`Division: ${num1} / ${num2} = ${quotient}`);
+
+console.log("Calculator Results (Negative Numbers):");
+// Addition of two negative numbers
+let negativeNum1 = -3;
+let negativeNum2 = -15;
+let sumOfNegatives = negativeNum1 + negativeNum2;
+console.log(`Addition: ${negativeNum1} + ${negativeNum2} = ${sumOfNegatives}`);
+
+//Subraction of two negative numbers
+let differenceOfNegatives = negativeNum1 - negativeNum2;
+console.log(`Subtraction: ${negativeNum1} - ${negativeNum2} = ${differenceOfNegatives}`);
+
+//Multiplication of two negative numbers
+let productOfNegatives = negativeNum1 * negativeNum2;
+console.log(`Multiplication: ${negativeNum1} * ${negativeNum2} = ${productOfNegatives}`);
+
+//Division of two negative numbers
+let quotientOfNegatives = negativeNum1 / negativeNum2;
+console.log(`Division: ${negativeNum1} / ${negativeNum2} = ${quotientOfNegatives}`);
+
+console.log("Calculator Results (Decimal Numbers):");
+// Addition of two decimal numbers
+let decimalNum1 = 3.3;
+let decimalNum2 = 15.4;
+
+//Addition of two decimal numbers
+let sumOfDecimals = decimalNum1 + decimalNum2;
+console.log(`Addition: ${decimalNum1} + ${decimalNum2} = ${sumOfDecimals}`);
+
+// Subtraction of two decimal numbers
+let differenceOfDecimals = decimalNum1 - decimalNum2;
+console.log(`Subtraction: ${decimalNum1} - ${decimalNum2} = ${differenceOfDecimals}`);
+
+// Multiplication of two decimal numbers
+let productOfDecimals = decimalNum1 * decimalNum2;
+console.log(`Multiplication: ${decimalNum1} * ${decimalNum2} = ${productOfDecimals}`);
+
+// Division of two decimal numbers
+let quotientOfDecimals = decimalNum1 / decimalNum2;
+console.log(`Division: ${decimalNum1} / ${decimalNum2} = ${quotientOfDecimals}`);
