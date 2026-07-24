fe2f0b3fa6ef9fd9f3a47f19e4571632b6812593
Neemarhh <nibukoye080@gmail.com>
2026-07-24 11:30:39 +0100

    Done with LAB-01

--- BASICS/LAB-01/code/variables.js
+++ BASICS/LAB-01/code/variables.js
@@ -7,17 +7,18 @@
 
 // TODO: Declare a variable called 'myName' and set it to your name (string)
 // Hint: use const because your name probably won't change!
-const myName = "Your Name Here";
+const myName = "Neemarhh";
 
 // TODO: Declare a variable called 'myAge' and set it to your age (number)
 // Hint: use let because your age changes every year!
-
+let myAge = 22;
 
 // TODO: Declare a variable called 'isStudent' and set it to true or false (boolean)
+const isStudent = true;
 
 
 // TODO: Declare a variable called 'favoriteLanguage' and set it to your favorite programming language (string)
-
+const favoriteLanguage = 'JavaScript'
 
 // ==========================================
 // 2. Output to Console
@@ -29,10 +30,16 @@ console.log("Name:", myName);
 // TODO: Use console.log() to output your age, student status, and favorite language
 // Make sure to include descriptive labels like the example above!
 
+console.log("age", myAge);
+console.log("student status:", isStudent);
+console.log("favorite language:", favoriteLanguage);
 
+
+ 
 // ==========================================
 // 3. Challenge (Optional)
 // ==========================================
 // Try to log a full sentence using your variables, like:
 // "Hi, I am Alice. I am 25 years old. It is true that I am a student."
 
+console.log(`hi, I am ${myName}. i am ${myAge} years old. It is ${isStudent} that i am a student.`);
